d11c9557a96271b25c5de837b991d460b7edc060
Nirajan <nirajanb89@gmail.com>
2016-11-02 17:10:32 +0545

    Postcss integrated in gulpfile and mixins changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 //Elixir gulpfile.js
 
 var elixir = require('laravel-elixir');
+require('laravel-elixir-postcss');
+require('postcss-import');
 
-elixir(function (mix) {
-    mix.sass('app.scss');
+elixir(function(mix) {
+    //mix.sass('app.scss');
+    mix.postcss('app.css', {
+        plugins:[
+            require('postcss-import'),
+            require('cssnano')
+        ]
+    });
 });
-
 
